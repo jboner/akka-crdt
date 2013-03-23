9461a196fd998decddd4994aa908716ef28f31f2
Jonas Bonér <jonas@jonasboner.com>
2013-03-23 12:37:09 +0100

    Added JSON serialization to AddSet.
    Signed-off-by: Jonas Bonér <jonas@jonasboner.com>

--- src/main/scala/com/typesafe/akka/crdt/AddSet.scala
+++ src/main/scala/com/typesafe/akka/crdt/AddSet.scala
@@ -4,16 +4,45 @@
 
 package com.typesafe.akka.crdt
 
+import scala.collection.immutable
+
+import play.api.libs.json._
+
 /**
- * Implements a CRDT G-Set.
- *
- * Set union is commutative and convergent; hence it is always safe to have
- * simultaneous writes to a set *which only allows addition*. You cannot remove an
- * element of a G-Set.
+ * Implements a CRDT 'Add Set' also called a 'G-Set'. You cannot remove an element of a G-Set.
  */
-case class AddSet[T](private[crdt] val state: Set[T] = Set.empty[T]) extends CRDTSet {
+case class AddSet[T](private[crdt] val state: Set[T] = Set.empty[T]) extends CRDTSet[T] {
+
+  val `type`: String = "g-set"
 
   def +(element: T): AddSet[T] = AddSet(state + element)
 
   def merge(that: AddSet[T]): AddSet[T] = AddSet(that.state ++ this.state)
+
+  def toSet: immutable.Set[T] = state
+}
+
+object AddSet {
+
+  implicit object stringFormat extends Format[AddSet[String]] {
+    def reads(json: JsValue): JsResult[AddSet[String]] = JsSuccess(AddSet(
+      (json \ "state").as[Set[String]]
+    ))
+
+    def writes(set: AddSet[String]): JsValue = JsObject(Seq(
+      "type" -> JsString(set.`type`),
+      "state" -> Json.toJson(set.state)
+    ))
+  }
+
+  implicit object jsValueFormat extends Format[AddSet[JsValue]] {
+    def reads(json: JsValue): JsResult[AddSet[JsValue]] = JsSuccess(AddSet(
+      (json \ "state").as[Set[JsValue]]
+    ))
+
+    def writes(set: AddSet[JsValue]): JsValue = JsObject(Seq(
+      "type" -> JsString(set.`type`),
+      "state" -> Json.toJson(set.state)
+    ))
+  }
 }
